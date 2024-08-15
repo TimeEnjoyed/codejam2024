4244584f13f848cc5759f8debf79c4abc119de9c
timeenjoyed <timeenjoyed.twitch@gmail.com>
2024-08-14 23:54:14 -0700

    added: login modal
    Co-authored-by: Mysty <evieepy@gmail.com>

--- html/src/lib/pages/TeamsBrowse.svelte
+++ html/src/lib/pages/TeamsBrowse.svelte
@@ -1,27 +1,28 @@
 <script lang="ts">
 
 import Page from "../components/Page.svelte";
-import {Button, Card} from "flowbite-svelte";
-import {getTeams} from "../services/services";
+import {Button, Card, Modal} from "flowbite-svelte";
+import {getTeams, joinPublicTeam} from "../services/services";
+import { loggedInStore, userStore } from '../stores/stores';
 import CodeJamTeam from '../models/team';
 import type TeamMember from '../models/TeamMember';
+import DiscordIcon from '../components/DiscordIcon.svelte';
 export const params: Record<string, never> = {};
 
 let teamData: CodeJamTeam | null = null;
 let teamMembers: TeamMember[] = [];
 let loading: boolean = true;
 let error: string | null = null;
-let allTeams: CodeJamTeam[]
+let allTeams: CodeJamTeam[] = [];
+let publicTeams: CodeJamTeam[] = [];
+let clickOutsideModal = false;
 
 async function loadData() {
     try {
         const response = await getTeams();
         allTeams = await response.json();  // Array of teams...
-        console.log(allTeams)
-
     } catch (err) {
         error = `Failed to load team data: ${err}`;
-        console.error(error)
     } finally {
         loading = false;
     }
@@ -29,34 +30,26 @@ async function loadData() {
 
 loadData();
 
+$: publicTeams = allTeams.filter(t => t.Visibility == "public" )
+
 </script>
     
 <Page>
     <Card size="md" class="w-full flex">
         <h3>Browse All Teams</h3>
-        
+
+
 		{#if loading}
 			<div class="p-4">Loading...</div>
 		{:else if error}
 			<div class="p-4 text-red-500">{error}</div>
-        
-		{:else if allTeams === null}
-            <div>
-                Error, please contact admin.
-            </div>
-
-		{:else if allTeams.length === 0}
-            <div>
-                Looks like you don't have any teams! Go to <a href="/#/">browse teams</a> to join one!
-            </div>
-        
         {:else}
+            {#each publicTeams as Team}
+            <Card size="xl" class="flex w-full p-8 px-4 py-z gap-3">
 
-            {#each allTeams as Team}
-            <Card size="xl" class="flex w-full p-8 px-4 py-6 space-y-3">
 				<center class="p-2">
 					<h4>Team {Team.Name}</h4>
-				</center>
+                </center>
                 <span>
 					<b>Visibility: </b>{Team.Visibility}
 				</span>
@@ -70,12 +63,21 @@ loadData();
 					<b>Description: </b>{Team.Description}
 				</span>
 
-
+                // only shows if user isnt a member
+                {#if $loggedInStore}
+                <Button on:click={()=>joinPublicTeam(Team.Id)}>Join</Button>
+                {:else}
+                <Button on:click={()=>(clickOutsideModal=true)}>Join</Button>
+                <Modal classBackdrop={"bg-gray-900/15 space-y-9"} bind:open={clickOutsideModal} autoclose outsideclose>
+                    <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400"><a href="/oauth/redirect">Login with Discord <DiscordIcon /></a>  to join a team!</p>
+                </Modal>
+                {/if}
 			</Card>
-
-
-            {/each}
-
+            {:else}
+                <div>
+                    Looks like you don't have any teams! Go to <a href="/#/">browse teams</a> to join one!
+                </div>
+        {/each}
 			
 		{/if}
 	</Card>
