4ec3410173032618debf93a8f54e0cfb018ed563
timeenjoyed <timeenjoyed.twitch@gmail.com>
2024-10-07 18:59:34 -0700

    WIP:  removeTeamMember table

--- html/src/lib/pages/TeamEdit.svelte
+++ html/src/lib/pages/TeamEdit.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import Page from '../components/Page.svelte';
-	import { Breadcrumb, BreadcrumbItem, Button, Card, Input } from 'flowbite-svelte';
+	import { Avatar, Breadcrumb, BreadcrumbItem, Button, Card, Input } from 'flowbite-svelte';
 	import { FontAwesomeIcon } from '@fortawesome/svelte-fontawesome';
 	import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 	import CodeJamTeam from '../models/team';
 	import { onMount } from 'svelte';
 	import { activeEventStore } from '../stores/stores';
-	import { getTeamById, postTeam } from '../services/services';
+	import { getTeamById, postTeam, removeMemberFromTeam } from '../services/services';
 	import Helper from 'flowbite-svelte/Helper.svelte';
 	import Radio from 'flowbite-svelte/Radio.svelte';
 	import Spinner from 'flowbite-svelte/Spinner.svelte';
@@ -16,28 +16,37 @@
 	import toast from 'svelte-french-toast';
 	import type CodeJamEvent from '../models/event';
 	import type TeamMember from '../models/TeamMember';
+	import {
+		Table,
+		TableBody,
+		TableBodyCell,
+		TableBodyRow,
+		TableHead,
+		TableHeadCell
+	} from 'flowbite-svelte';
 
 	export let params: any; // set by svelte-spa-router
 
-    // TODO: 
-    // Show each team member with a delete button next to username
-    // Successfully submit the form (update query to database.)
-    
-    let teamData: CodeJamTeam | null = null;
+	// TODO:
+	// Show each team member with a delete button next to username
+	// Successfully submit the form (update query to database.)
+
+	let teamData: CodeJamTeam | null = null;
 	let teamMembers: TeamMember[] = [];
 	let teamEvent: CodeJamEvent | null = null;
+	let avatarUrls: Record<string, string> = {};
 
-    let teamName: string = '';
-    let teamVisibility: string = '';
-    let teamAvailability: string = '';
-    let teamTechnologies: string = '';
-    let teamDescription: string = '';
-    
-    let loading: boolean = true; 
+	let teamName: string = '';
+	let teamVisibility: string = '';
+	let teamAvailability: string = '';
+	let teamTechnologies: string = '';
+	let teamDescription: string = '';
+
+	let loading: boolean = true;
 	let formData: CodeJamTeam | null = null;
 	let isSaving: boolean = false;
 	let teamCreated: boolean = false;
-    let error: string | null = null; 
+	let error: string | null = null;
 
 	let clearErrors: () => {};
 	let parseResponse: (response: object) => {};
@@ -77,45 +86,67 @@
 		}
 	}
 
-	// METHOD 1: OnMount
-	// this returns the team form with prefilled information
-	// removed onMount because there's no HTML required to preload.
-	// If the fetch-call (go-backedn) takes too long you will get race-issues sometimes. when use bind or interact with dom. TL;DR use onMOUNT to load data alaways.
-	// onMount(() => {
-	// 	if (params) {
-	// 		getTeamById(params.id).then((response) => {
-	// 			response.json().then((data) => {
-	// 				formData = data as CodeJamTeam;
-	// 			});
-	// 		});
-	// 	}
-	// });
+	function removeMember(teamId: string, memberId: string) {
+		if (confirm('Are you sure you want to remove this team member?')) {
+			// Call API to remove member
+			removeMemberFromTeam(teamId, memberId)
+				.then(() => {
+					// Update formData to reflect changes
+					teamMembers = teamMembers.filter((member) => member.UserId !== memberId);
+					toast.success('Member removed successfully');
+				})
+				.catch((err) => {
+					toast.error('Failed to remove member');
+					console.error(err);
+				});
+		}
+	}
 
-    async function loadData(id: string) {
-        try {
-            getTeamById(params.id).then((response) => {
+	async function getAvatarUrl(member: TeamMember): Promise<string> {
+		let ext = member.AvatarUrl.startsWith('a_') ? '.gif' : '.png';
+		return `https://cdn.discordapp.com/avatars/${member.ServiceUserId}/${member.AvatarUrl}${ext}`;
+	}
+
+	async function loadAvatarUrls() {
+        let members: TeamMember[] = [];
+
+		const promises = members.map(async (member) => {
+			const url = await getAvatarUrl(member);
+            avatarUrls[member.UserId] = url;
+		});
+		await Promise.all(promises);
+	}
+
+	function loadData(id: string) {
+		try {
+			getTeamById(params.id).then((response) => {
 				response.json().then((data) => {
 					formData = data as CodeJamTeam;
-                    teamData = data.Team;
-			        teamMembers = data.Members;
-			        teamEvent = data.Event;
-                    teamName = data.Team.Name;
-                    teamVisibility = data.Team.Visibility;
-                    teamAvailability = data.Team.Availability;
-                    teamTechnologies = data.Team.Technologies;
-                    teamDescription = data.Team.Description;
+					teamData = data.Team;
+					teamMembers = data.Members;
+					teamEvent = data.Event;
+					teamName = data.Team.Name;
+					teamVisibility = data.Team.Visibility;
+					teamAvailability = data.Team.Availability;
+					teamTechnologies = data.Team.Technologies;
+					teamDescription = data.Team.Description;
 				});
 			});
-        } catch (err) {
-            error = `Failed to load team data: ${err}`; 
-        } finally {
-            loading = false;
-        }
-    }
+		} catch (err) {
+			error = `Failed to load team data: ${err}`;
+		} finally {
+			loading = false;
+		}
+	}
 
-    $: if(params) {
-        loadData(params.id)
-    }
+	$: if (params) {
+		loadData(params.id);
+	}
+    onMount(() => {
+        loadAvatarUrls();
+    });
+$: loadAvatarUrls();
+
 </script>
 
 <Page>
@@ -126,57 +157,89 @@
 	</Breadcrumb>
 	<Card size="xl" class="w-full">
 		<div class="flex flex-row gap-8 my-8"></div>
-		Edit team form here:
 		{#if loading}
-            {console.log(formData, "line 107")}
+			{console.log(formData, 'line 107')}
 			<div class="p-4">Loading...</div>
 		{:else if error}
 			<div class="p-4 text-red-500">{error}</div>
-        {:else if formData !== null}
+		{:else if formData !== null}
+			<div class="flex flex-col gap-8 my-8">
+				<Form bind:clearErrors bind:parseResponse>
+					<FormField label="Team Name:" name="TeamName">
+						<Input bind:value={teamName}></Input>
+					</FormField>
+					<div>
+						<Radio name="team-type" bind:group={teamVisibility} value="public">Public Team</Radio>
+						<Helper class="ml-6 ">(If you want your team to be searchable.)</Helper>
+					</div>
+					<div>
+						<Radio name="team-type" bind:group={teamVisibility} value="private">Private Team</Radio>
+						<Helper class="ml-6">(Your team will be invite only)</Helper>
+					</div>
 
-            <div class="flex flex-col gap-8 my-8">
-                <Form bind:clearErrors bind:parseResponse>
-                    <FormField label="Team Name:" name="TeamName">
-                        <Input bind:value={teamName}></Input>
-                    </FormField>
-                    <div>
-                        <Radio name="team-type" bind:group={teamVisibility} value="public">Public Team</Radio>
-                        <Helper class="ml-6 ">(If you want your team to be searchable.)</Helper>
-                    </div>
-                    <div>
-                        <Radio name="team-type" bind:group={teamVisibility} value="private">Private Team</Radio>
-                        <Helper class="ml-6">(Your team will be invite only)</Helper>
-                    </div>
+					<FormField label="Your general availability:" name="TeamAvailability">
+						<Input bind:value={teamAvailability}></Input>
+					</FormField>
 
-                    <FormField label="Your general availability:" name="TeamAvailability">
-                        <Input bind:value={teamAvailability}>
-                        </Input>
-                    </FormField>
+					<!-- <MultiSelect id="multi-close" items={languages} bind:value={teamTechnologies} /> -->
+					<FormField label="Your technologies:" name="TeamTechnologies">
+						<Input bind:value={teamTechnologies}></Input>
+					</FormField>
 
-                    <!-- <MultiSelect id="multi-close" items={languages} bind:value={teamTechnologies} /> -->
-                    <FormField label="Your technologies:" name="TeamTechnologies">
-                        <Input
-                            bind:value={teamTechnologies}
-                        ></Input>
-                    </FormField>
+					<FormField label="What do you want out of this team?" name="Description">
+						<!-- <Label for="aboutTextArea">What do you want out of this team?</Label> -->
+						<Textarea bind:value={teamDescription} />
+					</FormField>
+				</Form>
 
-                    <FormField label="What do you want out of this team?" name="Description">
-                        <!-- <Label for="aboutTextArea">What do you want out of this team?</Label> -->
-                        <Textarea bind:value={teamDescription} />
-                    </FormField>
-                </Form>
+				<Button on:click={saveForm} disabled={isSaving}>
+					{#if isSaving}
+						<Spinner />
+					{:else}
+						Save
+					{/if}
+				</Button>
+			</div>
+		{:else}
+			<Spinner />
+		{/if}
+        {#if loading}
+			{console.log(formData, 'line 107')}
+			<div class="p-4">Loading...</div>
+		{:else if error}
+			<div class="p-4 text-red-500">{error}</div>
+		{:else if formData !== null}
+		<h2>Team Members</h2>
 
-                <Button on:click={saveForm} disabled={isSaving}>
-                    {#if isSaving}
-                        <Spinner />
-                    {:else}
-                        Save
-                    {/if}
-                </Button>
-            </div>
-        {:else}
-            <Spinner />
+        <Table>
+            <TableHead>
+                <TableHeadCell>Avatar</TableHeadCell>
+                <TableHeadCell>Username</TableHeadCell>
+                <TableHeadCell>Role</TableHeadCell>
+                <TableHeadCell>
+                    <span class="sr-only">Delete</span>
+                </TableHeadCell>
+            </TableHead>
+            <TableBody tableBodyClass="divide-y">
+                {#each teamMembers as member}
+                <TableBodyRow>
+                    <TableBodyCell><Avatar src={avatarUrls[member.UserId]} title={member.DisplayName} /></TableBodyCell>
+                    <TableBodyCell>{member.DisplayName}</TableBodyCell>
+                    <TableBodyCell>{member.TeamRole}</TableBodyCell>
+                    <TableBodyCell>
+                        {#if member.TeamRole !== 'owner'}
+                            {member.UserId}
+                            <Button on:click={() => teamData?.Id && member.UserId && removeMember(teamData.Id, member.UserId)} class="btn-remove text-red-500 hover:text-red-700" color="light">
+                        Remove
+                        </Button>
+                        {/if}
+
+                    </TableBodyCell>
+                </TableBodyRow>
+                {/each}
+
+            </TableBody>
+        </Table>
         {/if}
-
 	</Card>
 </Page>
